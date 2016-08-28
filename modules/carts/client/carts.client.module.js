2fd47ba0f51b8660b199e41cef86aa52329769d2
andry-baka <andry.baka@mitrais.com>
2016-08-28 10:06:27 +0800

    Clean unused project modules

--- modules/carts/client/carts.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('carts', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('carts.services');
-  app.registerModule('carts.routes', ['ui.router', 'core.routes', 'carts.services']);
-}(ApplicationConfiguration));
--- modules/vouchers/client/vouchers.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('vouchers', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('vouchers.services');
-  app.registerModule('vouchers.routes', ['ui.router', 'core.routes', 'vouchers.services']);
-}(ApplicationConfiguration));
