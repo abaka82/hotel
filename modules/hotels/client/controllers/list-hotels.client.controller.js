42c9ca73b1df99faef2b0de1b7c379d560e8ef3a
andry-baka <andry.baka@mitrais.com>
2016-08-30 13:59:29 +0800

    Add search by hotel name , by address , and by price range

--- modules/hotels/client/controllers/list-hotels.client.controller.js
+++ modules/hotels/client/controllers/list-hotels.client.controller.js
@@ -11,5 +11,62 @@
     var vm = this;
 
     vm.hotels = HotelsService.query();
+
+    vm.clearSearch = function() {
+      vm.search.$ = '';
+      vm.search.hotelName = '';
+      vm.search.address = '';
+      vm.filterModelMin.price = 0;
+      vm.filterModelMax.price = 1000000000000;
+    };
+
+    vm.byPriceRange = function (minValue, maxValue) {
+      if ((minValue === undefined) || (minValue === null)) minValue = Number.MIN_VALUE;
+      if ((maxValue === undefined) || (maxValue === null)) maxValue = Number.MAX_VALUE;
+
+      var numberPattern = /\d+/g;  // search for any digit price
+
+      return function predicateFunc(item) {
+        var isFound = false;
+
+        var price1 = item.hotels_comPrice.match(numberPattern);
+        if (price1 !== null) {
+          price1.forEach(function(entry) {
+            if ((minValue <= parseInt(entry, 10)) && (parseInt(entry, 10) <= maxValue)) {
+              isFound = true;
+            }
+          });
+        }
+
+        var price2 = item.agodaPrice.match(numberPattern);
+        if (price2 !== null) {
+          price2.forEach(function(entry) {
+            if ((minValue <= parseInt(entry, 10)) && (parseInt(entry, 10) <= maxValue)) {
+              isFound = true;
+            }
+          });
+        }
+
+        var price3 = item.booking_comPrice.match(numberPattern);
+        if (price3 !== null) {
+          price3.forEach(function(entry) {
+            if ((minValue <= parseInt(entry, 10)) && (parseInt(entry, 10) <= maxValue)) {
+              isFound = true;
+            }
+          });
+        }
+
+        var price4 = item.otherPrice.match(numberPattern);
+        if (price4 !== null) {
+          price4.forEach(function(entry) {
+            if ((minValue <= parseInt(entry, 10)) && (parseInt(entry, 10) <= maxValue)) {
+              isFound = true;
+            }
+          });
+        }
+
+        return isFound;
+      };
+    };
   }
 }());
